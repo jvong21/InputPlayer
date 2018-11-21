f69f238391823bbaf3363ae8fbaab9b86b6bb397
Justin Vongpanya <jj.vongp@gmail.com>
2018-11-21 09:38:56 -0700

    Solidified implementation for InputHold

--- KeyAction/Data/InputHold.cs
+++ KeyAction/Data/InputHold.cs
@@ -4,9 +4,18 @@ namespace InputActions.Data
 {
     class InputHold : Input
     {
+        public int HoldInMilliseconds { get; private set; }
+
         public InputHold(string key, int delayInMilliseconds) : base(key, delayInMilliseconds)
         {
 
+        }
+
+        public static InputHold CreateInputHoldWithHoldInMilliseconds(string key, int delayInMilliseconds, int holdInMilliseconds)
+        {
+            InputHold currentInputHold = new InputHold(key, delayInMilliseconds);
+            currentInputHold.HoldInMilliseconds = holdInMilliseconds; 
+            return currentInputHold; 
         }
     }
 }
